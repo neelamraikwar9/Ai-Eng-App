080879149a80b7a1a05d5f4c41a9297ed9e88169
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-08-03 11:08:40 +0530

    chore: added chat interface

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -10,8 +10,23 @@ const chat = () => {
         <Navbar />
       </div>
       <div className="chatContainer">
-        <h1>chat</h1>
-      </div>  
+        <div className="converse">
+          <h1>Converse</h1>
+        </div>
+
+        <div className="midChatCon">
+          <h2>Hi name</h2>
+          <i>
+            “You have to grow from the inside out. None can teach you, none can
+            make you spiritual. There is no other teacher but your own soul.”
+          </i>
+          <br />
+          <div className="typeInputCon">
+          
+            <input type="text" className="inp" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
